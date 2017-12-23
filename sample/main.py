51083564bf55639f7518990c1ca0c904f58261c5
V-Kopio <ville.kopio@gmail.com>
2017-12-23 23:03:17 +0200

    Added main file which at the moment just prints total lines for every commit

---
+++ sample/main.py
@@ -0,0 +1,25 @@
+import sys
+from git_data_collector import change_collector, branch_history
+
+def main(argv):
+    total_lines = 0
+    time_list = []
+    line_list = []
+
+    for line in branch_history():
+        if not line: continue
+
+        if line[0] != ' ':
+            commit_id, time = line.strip('\'').split(" ", 1)
+        else:
+            data = change_collector(line)
+
+            total_lines += data['insertions'] - data['deletions']
+
+            time_list.append(time)
+            line_list.append(total_lines)
+
+            print "%s, %s: %d lines" % (commit_id, time, total_lines)
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv))
