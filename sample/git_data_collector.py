24ef76764674465438fc1e7e94384ada7924f9d6
V-Kopio <ville.kopio@gmail.com>
2017-12-23 23:01:27 +0200

    Added git_data_collector which can show commit history and get data form git logs 'files changed, insertions, deletions' line

---
+++ sample/git_data_collector.py
@@ -0,0 +1,26 @@
+import subprocess
+import re
+
+def change_collector(string):
+    regex_matches = re.match(
+        r"( (?P<files>\d+) files? changed)"
+        r"(, (?P<insertions>\d+) insertions?\(\+\))?"
+        r"(, (?P<deletions>\d+) deletions?\(-\))?",
+        string
+    )
+
+    details = regex_matches.groupdict()
+
+    for detail in details:
+        details[detail] = int(details[detail]) if details[detail] else 0
+
+    return details
+
+def branch_history():
+    git = subprocess.Popen(
+        ["git", "log", "--shortstat", "--reverse", "--pretty=format:'%H %at'"],
+        stdout=subprocess.PIPE
+    )
+
+    out, _ = git.communicate()
+    return out.split('\n')
